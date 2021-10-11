e9ff14c8e5c6e0aab54e772f47b8f5320c920043
Alexa020204 <90634577+Alexa020204@users.noreply.github.com>
2021-10-11 10:42:43 -0500

    Delete 1.py

--- 1.py
+++
@@ -1,3 +0,0 @@
-#1.	Escribe un algoritmo o el respectivo diagrama de flujo para imprimir tu nombre y se le adicione un calificativo ingresado por el usuario. Por ejemplo: Carlos el crack o Juliana la mejor.
-calificativo=input("Ingrese el calificativo que quiera: ")
-print("Alexandra la",calificativo)
