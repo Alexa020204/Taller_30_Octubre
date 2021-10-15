210b55a11d9ae6485a85863eb348a1ece0978092
Alexa020204 <90634577+Alexa020204@users.noreply.github.com>
2021-10-15 10:57:23 -0500

    Update 16.py

--- 16.py
+++ 16.py
@@ -2,4 +2,4 @@
 
 minutos=int(input("Ingrese los minutos que desea convertir a horas: "))
 horas=minutos/60
-print(minutos," minutos = ",horas," horas")
+print(minutos," minutos = {:.2f} horas".format(horas))
