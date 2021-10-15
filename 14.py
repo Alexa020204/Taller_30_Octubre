6364dae7718b4320163f900282205c0725ce2dbb
Alexa020204 <90634577+Alexa020204@users.noreply.github.com>
2021-10-15 10:44:40 -0500

    Update 14.py

--- 14.py
+++ 14.py
@@ -1,6 +1,5 @@
 #14.	Escribe un algoritmo o el respectivo diagrama de flujo que determine la energía (en Julios) de un objeto si se conoce la masa de un objeto (en kg) y la velocidad de la luz (en m/s).
-import numpy as np
-masa=int(input("Ingrese la masa del objeto en kg: "))
-velocidad = 299792458 #m/s
-Energia=float(masa*(velocidad**2))
-print(format(Energia,'.1E'))
+masa=float(input("Ingrese la masa del objeto en kg: "))
+velocidad = float(input("Ingrese la velocidad del objeto: "))
+Energia=(masa*velocidad)/2
+print(format(Energia,"J"))
