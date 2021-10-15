e0fdc8f38c75b5ccaafebabd73e1f9284591491e
Alexa020204 <90634577+Alexa020204@users.noreply.github.com>
2021-10-15 10:45:42 -0500

    Add files via upload

---
+++ trabajo.py
@@ -0,0 +1,13 @@
+#52.Construye un algoritmo y el respectivo diagrama de flujo
+#que permita leer una cantidad variable de números y nos indique cuantos
+#fueron mayores a 100 y cuántos menores a 100.
+a=0
+b=0
+pn=int(input("Ingrese cuantos numeros quiere ingresar: "))
+for i in range(pn):
+    p1=int(input("Ingrese un número: "))
+    if p1>=100:
+      a+=1
+    else:
+        b+=1
+print("números mayores a 100: ",a,"\nnúmeros menores a 100: ",b)
